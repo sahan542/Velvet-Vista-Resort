d654a24de5984ede41ed15c53bb74f3b2db5ce98
sahan542 <sahanrashmika542@gmail.com>
2024-01-10 04:32:54 +0530

    page carousul

--- Front-End/velvet-vista/src/components/home/Home.jsx
+++ Front-End/velvet-vista/src/components/home/Home.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import MainHeader from '../layout/MainHeader'
 import Parallax from '../common/Parallax'
 import HotelService from './../common/HotelService';
+import RoomCarousel from '../common/RoomCarousel';
 
 const Home = () => {
   return (
     <section>
       <MainHeader/>
       <section className="container">
+        <RoomCarousel/>
         <Parallax/>
+        <RoomCarousel/>
         <HotelService/>
         <Parallax/>
+        <RoomCarousel/>
       </section>
     </section>
   )
