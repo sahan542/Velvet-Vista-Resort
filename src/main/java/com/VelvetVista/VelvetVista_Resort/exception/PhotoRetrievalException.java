2d7fb4674ebffc40b9b1372ab0fd8ae59a871ac2
sahan542 <sahanrashmika542@gmail.com>
2024-05-28 19:22:54 +0530

    updated photoretrieval exception

--- src/main/java/com/VelvetVista/VelvetVista_Resort/exception/PhotoRetrievalException.java
+++ src/main/java/com/VelvetVista/VelvetVista_Resort/exception/PhotoRetrievalException.java
@@ -1,7 +1,12 @@
 package com.VelvetVista.VelvetVista_Resort.exception;
 
+import java.sql.SQLException;
+
 public class PhotoRetrievalException extends RuntimeException {
     public PhotoRetrievalException(String message) {
-        super();
+        super(message);
     }
 }
+    //public PhotoRetrievalException(String message, SQLException e) {
+    //    super(message);
+  //  }
